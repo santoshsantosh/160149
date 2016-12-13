b0d1d82058af2c8990420505a6c994e5014fc2dd
santoshsantosh <yse160149@gmail.com>
2016-12-13 10:30:43 +0900

    Aloha 4 and 5 やりました。

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,10 +1,16 @@
 public class Aloha{
 	public static void main(String[] args) {
-		for(int i=0;i<20;i++){
-		
-		
-			System.out.println(args[0]);
-		
+		while(true){
+			// System.out.println("input data>");
+			System.out.println("input name >");
+			String name=new java.util.Scanner(System.in).nextLine();
+			
+			if(name.equals("exit")){
+				System.out.println("おわりました");
+				break;
+			}
+			System.out.println(name);
+	}
 	}
   }
-  }
+  
