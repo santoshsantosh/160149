4d6f7f208e25e7359538066b6a517ce7167b014c
santoshsantosh <yse160149@gmail.com>
2016-12-13 10:08:05 +0900

    java 3 を　つくりました。

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,16 +1,10 @@
 public class Aloha{
 	public static void main(String[] args) {
 		for(int i=0;i<20;i++){
-		System.out.println("Aloha");
-		if(i%4==0){
-
-		}else{
-			System.out.println("Namaste");
-		}
+		
+		
+			System.out.println(args[0]);
+		
 	}
-	System.out.println("input name");
-	String name=new java.until.scanner(System in).next line();
-
-
-}
-}
+  }
+  }
