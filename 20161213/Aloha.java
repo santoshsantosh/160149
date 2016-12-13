5d43ce6e65e4625fc2818d8e19cbe691108288c5
santoshsantosh <yse160149@gmail.com>
2016-12-13 09:55:38 +0900

    (4 回に1回)Aloha.javaをつくりました。

---
+++ 20161213/Aloha.java
@@ -0,0 +1,16 @@
+public class Aloha{
+	public static void main(String[] args) {
+		for(int i=0;i<20;i++){
+		System.out.println("Aloha");
+		if(i%4==0){
+
+		}else{
+			System.out.println("Namaste");
+		}
+	}
+	System.out.println("input name");
+	String name=new java.until.scanner(System in).next line();
+
+
+}
+}
