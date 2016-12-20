5cb1b108e8610f88cf0f315bde3b931f49db573d
santoshsantosh <yse160149@gmail.com>
2016-12-20 10:44:37 +0900

    AlohaWindow.java add

---
+++ 20161220/AlohaWindow.java
@@ -0,0 +1,9 @@
+ import java.awt.*;
+ import javax.swing.*;
+ public class AlohaWindow{
+ public static void main(String[] args) {
+	JFrame farme=new JFrame();
+	farme.setSize(200,200);
+	farme.setVisible(true);
+}
+}
