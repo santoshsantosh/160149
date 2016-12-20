06e9a875561bc338492a62721dea418d0f67e9b7
santoshsantosh <yse160149@gmail.com>
2016-12-20 11:11:03 +0900

    AlohaWindow.java add

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -3,7 +3,9 @@
  public class AlohaWindow{
  public static void main(String[] args) {
 	JFrame farme=new JFrame();
-	farme.setSize(200,200);
+	farme.setSize(400,400);
+	farme.setTitle("Sharma Santosh");
+	farme.setDefultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	farme.setVisible(true);
 }
 }
