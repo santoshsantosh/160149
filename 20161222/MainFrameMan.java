247d2e342d678c01e3bf1afd1adc9895e4e661f7
santoshsantosh <yse160149@gmail.com>
2016-12-22 10:14:37 +0900

    MainFrameMan.java added.

--- 20161222/MainFrameMan.java
+++ 20161222/MainFrameMan.java
@@ -7,12 +7,18 @@ import java.awt.*;
  }
  class FrameMan{
  	JFrame frame;
+ 	JPanel panel;
+ 	JLabel label;
  	public FrameMan(){
  		System.out.println("Aloha2");
  		frame=new JFrame();
  		frame.setLocation(200,200);
 		frame.setSize(300,300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel=new JPanel();
+		label=new JLabel("ALOHA");
+		panel.add(label);
+		frame.add(panel);
 		frame.setVisible(true);
  	}
  }
