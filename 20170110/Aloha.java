aad4c2e6091a0076db5a706c6d74c0d2c7142ba7
santoshsantosh <yse160149@gmail.com>
2017-01-10 11:28:51 +0900

    Aloha Done

---
+++ 20170110/Aloha.java
@@ -0,0 +1,10 @@
+public class Aloha{
+	public static void main(String[] args) {
+		for(int i=0;i<20;i++){
+		System.out.println("Aloha");
+		if(i%3==0){
+			System.out.println("Namaste");
+		}
+	}
+	}
+}
---
+++ 20170110/Alohaman.java
@@ -0,0 +1,5 @@
+public class Alohaman{
+	public static void main(String[] args) {
+	
+	}
+}	
